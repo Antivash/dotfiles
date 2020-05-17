c818db189ae4f821bc7354323d716f25d3a8fa23
Antivash <sterling.r.j.s@gmail.com>
2020-05-17 11:35:55 -0500

    Adding Termite Stuff

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ mkdir -p ~/.config/nvim/configs
 mkdir -p ~/.config/zsh
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/kitty
+mkdir -p ~/.config/gtk-3.0
+mkdir -p ~/.config/termite
 mkdir -p ~/.config/lf
 
 # os.symlink the dots 
 ln -s /home/git/dotfiles/aliases.zshrc ~/.config/zsh
 ln -s /home/git/dotfiles/bro ~/.bro
 ln -s /home/git/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -s /home/git/dotfiles/fix-colors.zshrc ~/.config/zsh
 ln -s /home/git/dotfiles/folding.vim ~/.config/nvim/configs
-ln -s /home/git/dotfiles/foldtext.vim ~/.config/nvim/configs
 ln -s /home/git/dotfiles/functions.vim ~/.config/nvim/configs
 ln -s /home/git/dotfiles/init.vim ~/.config/nvim
 ln -s /home/git/dotfiles/jpg-to-png.zshrc ~/.config/zsh
@@ -26,12 +26,11 @@ ln -s /home/git/dotfiles/nvm.zshrc ~/.config/zsh
 ln -s /home/git/dotfiles/paths.zshrc ~/.config/zsh
 ln -s /home/git/dotfiles/plugin-settings.vim ~/.config/nvim/configs
 ln -s /home/git/dotfiles/plugins.vim ~/.config/nvim/configs
-ln -s /home/git/dotfiles/plugins.zshrc ~/.config/zsh
 ln -s /home/git/dotfiles/powerlevel10k.zshrc ~/.config/zsh
-ln -s /home/git/dotfiles/ruby.zshsrc ~/.config/zsh
-ln -s /home/git/dotfiles/themes.zshrc ~/.config/zsh
 ln -s /home/git/dotfiles/zshrc ~/.zshrc
 ln -s /home/git/dotfiles/gitconfig ~/.gitconfig
+ln -s /home/git/dotfiles/termite.config ~/.config/termite/config
+ln -s /home/git/dotfiles/gtk.css ~/.config/gtk-3.0
 # Setting Kitty Terminal Color Scheme
 ln -s /home/git/dotfiles/themes/kitty/Monokai_Soda.conf ~/.config/kitty/theme.conf
 # Change Powerlevel10k Theme
